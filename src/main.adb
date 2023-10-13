b1a9f6050dd6a184ae2a209f588f45443c30d801
Mats <84991666+MatsSyvertsen@users.noreply.github.com>
2023-10-13 14:49:39 +0200

    Setup format on save + editorconfig

--- src/main.adb
+++ src/main.adb
@@ -1,7 +1,9 @@
-Procedure Main with Priority => 0 is
+procedure Main with
+  Priority => 0
+is
 begin
-    -- Do a loop in main otherwise the micro:bit will constantly reset
-    loop
-        null;
+   -- Do a loop in main otherwise the micro:bit will constantly reset
+   loop
+      null;
    end loop;
 end Main;
