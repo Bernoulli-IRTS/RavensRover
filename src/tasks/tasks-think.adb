26ecdf45ca410eceb7eac3cd12b1c019a408efe0
Ruuebn <112164477+Ruuebn@users.noreply.github.com>
2023-11-05 18:41:58 +0100

    Add obstacle detection

--- src/tasks/tasks-think.adb
+++ src/tasks/tasks-think.adb
@@ -6,34 +6,30 @@ package body Tasks.Think is
 
    task body Think is
       Start : Time := Clock;
+      Obstacle : Where_Obstacle;
    begin
+      -- Initial delay, decreases the chance of overcurrent from testing
+      delay(3.0);
       loop
          Start := Clock;
-         delay (3.0);
-         Act.Set_Rotation (2_048);
-         delay (0.5);
-         Act.Stop;
-         delay (3.0);
-         Act.Set_Rotation (-2_048);
-         delay (0.5);
-         Act.Stop;
-         delay (0.5);
-         Act.Set_Forward (1_048);
-         delay (0.5);
-         Act.Stop;
-         delay (0.5);
-         Act.Set_Forward (-1_048);
-         delay (0.5);
-         Act.Stop;
-         delay (0.5);
-         Act.Set_Right (1_048);
-         delay (0.5);
-         Act.Stop;
-         delay (0.5);
-         Act.Set_Right (-1_048);
-         delay (0.5);
-         Act.Stop;
-         delay (0.5);
+         Obstacle := Is_Obstacle_Ahead;
+
+         if Obstacle = Both then
+            Act.Set_Rotation(2_048);
+            delay(0.5);
+            Act.Stop;
+            delay(3.0);
+         elsif Obstacle = Left then
+            Act.Set_Rotation(2_048);
+            delay(0.5);
+            Act.Stop;
+            delay(3.0);
+         elsif Obstacle = Right then
+            Act.Set_Rotation(-2_048);
+            delay(0.5);
+            Act.Stop;
+            delay(3.0);
+         end if;
 
          delay until Start + Milliseconds (10);
       end loop;
@@ -45,11 +41,11 @@ package body Tasks.Think is
       Right_Sensor : HCSR04Distance := Sense.Get_Front_Right_Distance;
    begin
       -- Returns enumerator type according to what the sensor reads
-      if Left_Sensor > 10.0 and Right_Sensor > 10.0 then
+      if Left_Sensor < 10.0 and Right_Sensor < 10.0 then
          return Both;
-      elsif Left_Sensor > 10.0 then
+      elsif Left_Sensor < 10.0 then
          return Left;
-      elsif Right_Sensor > 10.0 then
+      elsif Right_Sensor < 10.0 then
          return Right;
       end if;
       return None;
