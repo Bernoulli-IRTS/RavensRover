a1afa9d037dd297677f338db79e9b34673468fa0
Mats <84991666+MatsSyvertsen@users.noreply.github.com>
2023-11-14 09:17:13 +0100

    Add accelerometer based radio control

--- radiotransmitter/src/main.adb
+++ radiotransmitter/src/main.adb
@@ -1,16 +1,26 @@
 with Ada.Real_Time;          use Ada.Real_Time;
+with MicroBit.Accelerometer;
+with MicroBit.Buttons;       use MicroBit.Buttons;
+with MicroBit.Console;       use MicroBit.Console;
 with MicroBit.Radio;
 with MicroBit.IOsForTasking; use MicroBit.IOsForTasking;
-with MicroBit.Buttons;       use MicroBit.Buttons;
 with Radio;                  use Radio;
 with Controller;
+
+with LSM303AGR; use LSM303AGR;
 
 procedure Main with
   Priority => 0
 is
+   Start        : Time;
    Button_A_Pin : constant := 5;
    -- Default packet
-   Packet : MicroBit.Radio.RadioData;
+   Packet           : MicroBit.Radio.RadioData;
+   Data             : All_Axes_Data;
+   Forward          : Integer;
+   Rotation         : Integer;
+   Button_A_Pressed : Boolean;
+   Button_B_Pressed : Boolean;
 begin
    MicroBit.Radio.Setup
      (RadioFrequency => Radio.RadioFrequency, Length => Radio.Length,
@@ -23,14 +33,43 @@ begin
    Packet.Protocol := Radio.Protocol;
 
    loop
-      if State (Button_A) = Pressed then
-         Controller.Transmit_Move (Packet, 4_095, 0, 0);
-      end if;
+      begin
+         Start := Clock;
 
-      if State (Button_B) = Pressed then
-         Controller.Transmit_Move (Packet, -4_095, 0, 0);
-      end if;
+         Button_A_Pressed := State (Button_A) = Pressed;
+         Button_B_Pressed := State (Button_B) = Pressed;
 
-      delay To_Duration (Milliseconds (10));
+         if Button_A_Pressed or Button_B_Pressed then
+            -- Get accelerometer data
+            Data := MicroBit.Accelerometer.AccelData;
+
+            -- Calculate Forward from Z axis if over 80 (threshold)
+            if abs Data.Z > 80 then
+               Forward := -Integer'Min (Integer (Data.Z) * 16, 4_095);
+            else
+               Forward := 0;
+            end if;
+
+            -- Calculate Rotation from X axis if over 80 (threshold)
+            if abs Data.X > 80 then
+               Rotation := -Integer'Min (Integer (Data.X) * 16, 4_095);
+            else
+               Rotation := 0;
+            end if;
+
+            Controller.Transmit_Move
+              (Packet, MoveSpeed (Forward),
+               -- If only A or B is held, move left/right
+               (if Button_A_Pressed /= Button_B_Pressed then
+                  (if Button_B_Pressed then 2_048 else -2_048)
+                else 0),
+               MoveSpeed (Rotation));
+         end if;
+
+         delay until Start + Milliseconds (10);
+      exception
+         when others =>
+            null; -- Ignore exceptions for now
+      end;
    end loop;
 end Main;
